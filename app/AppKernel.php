290479ece2b82b1b27989484ce43e84a72b99335
Ivan Berezhnov <ivan.berezhnov@sanoma.com>
2018-07-22 21:13:37 +0300

    Add MarkdownBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
