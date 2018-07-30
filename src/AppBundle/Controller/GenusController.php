c37788b26b3e436949b28941a08518be6af1506b
Ivan Berezhnov <ivan.berezhnov@icloud.com>
2018-07-30 08:49:35 +0300

    Add db connection and db

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,17 +3,61 @@
 namespace AppBundle\Controller;
 
 
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use AppBundle\Entity\Genus;
 
 class GenusController extends Controller
 {
+
+  /**
+   * @Route(path="/genus/new")
+   */
+  public function newAction()
+  {
+    $genus = new Genus();
+    $genus->setName('Octopus'.rand(1, 100));
+    $genus->setSubFamily('Octopodinea');
+    $genus->setSpeciesCount(rand(100, 99999));
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($genus);
+    $em->flush();
+
+    return new Response('<html><body>Genus created!</body></html>');
+  }
+
+  /**
+   * @Route(path="/genus")
+   */
+  public function listAction()
+  {
+      $em = $this->getDoctrine()->getManager();
+      $genuses = $em->getRepository('AppBundle:Genus')
+        ->findAll();
+
+      return $this->render('genus/list.html.twig', [
+        'genuses' => $genuses,
+      ]);
+  }
+
     /**
-     * @Route(path="/genus/{genusName}")
+     * @Route(path="/genus/{genusName}", name="genus_show")
      */
     public function showAction($genusName)
     {
+
+        $em = $this->getDoctrine()->getManager();
+        $genus = $em->getRepository('AppBundle:Genus')
+          ->findOneBy(['name' => $genusName]);
+
+        if (!$genus) {
+          throw $this->createNotFoundException('No genus found!');
+        }
+
+      /*
         $funFacts = 'Octopuses can change the color of their body in just *three-tenths* of a second!';
 
         $cache = $this->get('doctrine_cache.providers.my_cache');
@@ -28,11 +72,10 @@ class GenusController extends Controller
                 ->transform($funFacts);
             $cache->save($key, $funFacts);
         }
-
+      */
 
         return $this->render('genus/show.html.twig', [
-            'name' => $genusName,
-            'funFacts' => $funFacts
+            'genus' => $genus,
         ]);
     }
 
---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="GenusRepository")
+ * @ORM\Table(name="genus")
+ */
+class Genus
+{
+  /**
+   * @ORM\Id
+   * @ORM\GeneratedValue(strategy="AUTO")
+   * @ORM\Column(type="integer")
+   */
+  private $id;
+
+  /**
+   * @ORM\Column(type="string")
+   */
+  private $name;
+
+  /**
+   * @ORM\Column(type="string")
+   */
+  private $subFamily;
+
+  /**
+   * @ORM\Column(type="integer")
+   */
+  private $speciesCount;
+
+  /**
+   * @ORM\Column(type="string", nullable=true)
+   */
+  private $funFact;
+
+  /**
+   * @return mixed
+   */
+  public function getName()
+  {
+    return $this->name;
+  }
+
+  /**
+   * @param mixed $name
+   */
+  public function setName($name)
+  {
+    $this->name = $name;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getSubFamily()
+  {
+    return $this->subFamily;
+  }
+
+  /**
+   * @param mixed $subFamily
+   */
+  public function setSubFamily($subFamily)
+  {
+    $this->subFamily = $subFamily;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getSpeciesCount()
+  {
+    return $this->speciesCount;
+  }
+
+  /**
+   * @param mixed $speciesCount
+   */
+  public function setSpeciesCount($speciesCount)
+  {
+    $this->speciesCount = $speciesCount;
+  }
+
+  /**
+   * @return mixed
+   */
+  public function getFunFact()
+  {
+    return $this->funFact;
+  }
+
+  /**
+   * @param mixed $funFact
+   */
+  public function setFunFact($funFact)
+  {
+    $this->funFact = $funFact;
+  }
+
+  public function getUpdatedAt()
+  {
+    return new \DateTime('-' . rand(0, 100) . ' days');
+  }
+
+}
